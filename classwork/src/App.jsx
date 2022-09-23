fa89c39635702c06e731d03c18f948c7ea0034d3
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-23 18:50:02 +0530

    Update App.jsx

--- classwork/src/App.jsx
+++ classwork/src/App.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 class App extends React.Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      name:""
+      name: "",
+    };
+  }
+  joy = (e) => {
+    if (e.target.checked === false) {
+      this.setState({ name: this.state.name.replace(e.target.value, "") });
+    } else if (e.target.checked === true) {
+      this.setState({ name: this.state.name + `${e.target.value} ` });
     }
-  }
-joy = (e)=>{
-  if( e.target.checked=== false){
-    this.setState({name :this.state.name.replace(e.target.value,"")})
-  }
- else if(e.target.checked===true){   
-  this.setState({name :this.state.name+`${ e.target.value} `})
- }
-}
+  };
   render() {
     return (
       <div>
         <h2>Q. What are the foods that you like ?</h2>
         <h2>A. {this.state.name}</h2>
-        <input onChange={this.joy} type="checkbox" id="fruits" value="Fruits"/>
+        <input onChange={this.joy} type="checkbox" id="fruits" value="Fruits" />
         <label htmlFor="fruits">Fruits</label>
         <br />
-        <input onChange={this.joy} type="checkbox" id="biryani" value="Biryani"/>
+        <input
+          onChange={this.joy}
+          type="checkbox"
+          id="biryani"
+          value="Biryani"
+        />
         <label htmlFor="biryani">Biryani</label>
         <br />
         <input onChange={this.joy} type="checkbox" id="veg" value="Mix-Veg" />
