bc4f74285d8668b25d20ac3c8e0c1a4650018f4b
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-27 00:03:21 +0530

    Update Hook.jsx

--- classwork/src/Components/Hook.jsx
+++ classwork/src/Components/Hook.jsx
@@ -1,26 +1,75 @@
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
+import "./Hook.css";
 export default function Hook() {
-let[name,setName]= useState({firstname:"",lastname:""})
-let[item,setitem]=useState([])
+  let [name, setName] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    team: "",
+  });
+  let [para, setpara] = useState([]);
+
+  let addpara = (e) => {
+    e.preventDefault();
+    setpara([...para, name]);
+    setName({firstname:"",lastname:"",email:"",team:""})
+    console.log(para)
+  };
   return (
-    <form >
-        <input type="text" onChange={(e)=>{
-            setName({firstname:e.target.value})
+    <>
+      <form>
+        <div className="form_class">
+            <h3>First Name</h3>
+          <input value={name.firstname}
+            type="text"
+            onChange={(e) => {
+              setName({ ...name,firstname: e.target.value });
+            }}
+          />  <h3>last Name</h3>
+          <input
+          value={name.lastname}
+            type="text"
+            onChange={(e) => {
+              setName({ ...name,lastname: e.target.value });
+            }}
+          />
+          <h3>email</h3>
+          <input
+          value={name.email}
+            type="text"
+            onChange={(e) => {
+              setName({...name, email: e.target.value });
+            }}
+          />
+          <h3>team</h3>
+          <input
+            type="text"
+            value={name.team}
+            onChange={(e) => {
+              setName({...name, team: e.target.value });
+            }}
+          />
+          <button onClick={addpara}>Submit</button>
+        </div>
+      </form>
 
-        }} value={name.firstname} />
-        <input type="text" onChange={(e)=>setName({lastname:e.target.value})}  value={name.lastname}/>
-        <button onClick={()=>{
-
-        }}>submit</button>
-
-
-        <h1>Your First Name is: {name.firstname}</h1>
-        <h1>Your Last Name is: {name.lastname}</h1>
-        <h1>{JSON.stringify(name)}</h1>
-    </form>
-
-
-
-  )
+      {para.map((item) => {
+        return (
+          <div className="content">
+            <p>{item.firstname}</p>
+            <p>{item.lastname}</p>
+            <p>{item.email}</p>
+            <p>{item.team}</p>
+            <button onClick={(e)=>{
+                
+            }}>edit</button>
+            <button   onClick={() => {
+              let filteredItems = para.filter((value) => value !== item);
+              setpara([...filteredItems])
+             }}>delete</button>
+          </div>
+        );
+      })}
+    </>
+  );
 }
