b4033f9aadb0012868221fff69cedc2df3609bf8
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-28 23:26:30 +0530

    Create Keyboard.jsx

---
+++ classwork/src/Components/Keyboard.jsx
@@ -0,0 +1,34 @@
+import React,{useState} from "react";
+
+let  Keyboard = ()=>{
+
+    let[btn,setBtn]= useState("")
+    let add =(e)=>{
+        setBtn(btn+e.target.innerHTML)
+    }
+    return(
+        <>
+        <input type="text" value={btn}/>
+        <button onClick={add}>q</button>
+        <button onClick={add}>w</button>
+        <button onClick={add}>e</button>
+        <button onClick={add}>r</button>
+        <button onClick={add}>t</button>
+        <button onClick={add}>u</button>
+        <button onClick={add}>i</button>
+        <button onClick={add}>o</button>
+        <button onClick={add}>p</button>
+        <button onClick={add}>a</button>
+        <button onClick={add}>s</button>
+        <button onClick={add}>d</button>
+        <button onClick={add}>f</button>
+        <button onClick={add}>g</button>
+        <button onClick={add}>h</button>
+        <button onClick={add}>j</button>
+        <button onClick={add}>k</button>
+        <button onClick={add}>l</button>
+        </>
+    )
+}
+
+export default Keyboard
