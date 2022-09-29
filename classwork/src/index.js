3318c24d176b3ebdac1f5fca8acb5366b76e2cdf
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-29 09:08:34 +0530

    Update index.js

--- classwork/src/index.js
+++ classwork/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hook from './Components/Hook'
-import Keyboard from './Components/Keyboard';
+// import Hook from './Components/Hook'
+// import Keyboard from './Components/Keyboard';
+import Useeffect from './Components/Useeffect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <Hook /> */}
-    < Keyboard />
-  </React.StrictMode>
+  // <React.StrictMode>
+    < Useeffect/>
+  /* </React.StrictMode> */
 );
 
 reportWebVitals();
