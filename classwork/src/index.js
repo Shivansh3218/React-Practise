f993dfce8ae4edfd6e17f70a8db95ce56acd19dc
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-03 21:06:37 +0530

    Update index.js

--- classwork/src/index.js
+++ classwork/src/index.js
@@ -5,11 +5,13 @@ import reportWebVitals from './reportWebVitals';
 // import Hook from './Components/Hook'
 // import Keyboard from './Components/Keyboard';
 import Useeffect from './Components/Useeffect';
+import Todo from './Components/Todo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    < Useeffect/>
+    // < Useeffect/>
+    <Todo />
   /* </React.StrictMode> */
 );
 
