3192826095503148ec90e05e5f6ffb0fe465cc59
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-06 18:56:42 +0530

    Update LoginPage.jsx

--- classwork/src/Components/LoginPage.jsx
+++ classwork/src/Components/LoginPage.jsx
@@ -1,60 +1,28 @@
-import React,{useState} from "react";
-// import { LoginPage } from "./LoginPage";
+import React, { useState } from "react";
+import Welcome from "./Welcome";
 
-export function LoginPage(){
+export function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username,setUsername]=useState("");
-    const [password,setPassword]=useState("");
-    // const [datas,setDatas]=useState("");
+  const handleSubmit = (e) => {
+    const newData = { username: username, password: password };
 
-    // const getUsername=localStorage.getItem("userData");
-    // const getPassword=localStorage.getItem("userpswd")
-
-    console.log(username)
-    console.log(password)
-
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        // const newData={username:username,password:password};
-        // setDatas([...datas,newData])
-        // console.log(datas);
-        // if(username.current.value===username && password.current.value===password){
-        //     localStorage.setItem("userData",username)
-        //     localStorage.setItem("userpswd",password)
-        // }
-    }
-
-
-    return(
-        <>
-        <div>
-
-          
+  };
+  return (
+  <>
+  <form action="./Welcome.jsx">
+    <label htmlFor="Username" >Enter Username</label>
+    <input className="user" onChange={(e)=>{
+      setUsername(e.target.value)
+    }} type="text" />
+    <label htmlFor="password">Enter Password</label>
+    <input type="password" />
+    <button onClick={(e)=>{
         
-            <div >
-                <form  onSubmit={handleSubmit}>
-                    <fieldset>
-                        <legend>Login</legend>
-                    <div>
-                        <label htmlFor="username">Username : </label><br />
-                        <input type="text" id="username" placeholder="Enter username"
-                        onChange={(e)=>setUsername(e.target.value)} value={username}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="pswd">Password : </label><br />
-                        <input type="password" id="pswd" placeholder="Enter password" 
-                        onChange={(e)=>setPassword(e.target.value)} value={password}
-                        />
-                    </div>
-                    <div className="btn_div">
-                    <button type="submit" className="submit_btn">Submit</button>
-                    </div>
-                    </fieldset>
-                </form>
-            </div>
-           
-            </div>
-        </>
-    )
+    }}> submit</button>
+
+  </form>
+    </>
+  );
 }
