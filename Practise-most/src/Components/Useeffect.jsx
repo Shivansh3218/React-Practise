f8b75e7418b772169170ffc255c56f6264433d6e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-09 20:42:25 +0530

    React Router Practise

--- Practise-most/src/Components/Useeffect.jsx
+++ Practise-most/src/Components/Useeffect.jsx
@@ -1,5 +1,6 @@
 import React ,  {useState,useEffect} from 'react'
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 function Useeffect() {
 // let [posts , setPosts]=useState([]);
@@ -23,16 +24,25 @@ useEffect(()=>{
 },[])
   return (
     <div>
+           <ul>
+          <li>
+            <Link to="/Hook">Hook</Link></li>
+            <li><Link to="/">Checkbox</Link></li>
+            <li><Link to="/Keyboard">Keyboard</Link></li>
+            <li> <Link to="/LoginPage">LoginPage</Link></li>
+            <li> <Link to="/Useeffect">Useeffect</Link></li>
+            <li> <Link to="/Welcome">Welcome</Link></li>
+        </ul>
       <h1>USE EFFECT </h1>
    {
-    post.map((post)=>{  //-----------------Doubt why use paranthesis here 
+    post.map((post)=>(//-----------------Doubt why use paranthesis here 
         // console.log(post.body)
         <div >
             <h1 key={post.id}>{post.title}</h1>
             {/* <p>{post.body}</p> */}
         </div>
     
-    })
+    ))
    }
     </div>
 
