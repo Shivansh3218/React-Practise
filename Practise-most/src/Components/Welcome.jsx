ac11a6efc7b1a3f66a783a3e5b45f692bd17543e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-09 20:42:20 +0530

    React Router Practise

--- Practise-most/src/Components/Welcome.jsx
+++ Practise-most/src/Components/Welcome.jsx
@@ -1,7 +1,17 @@
 import React from "react"
+import {Link} from 'react-router-dom'
 function Welcome(props){
     return(
         <>
+             <ul>
+          <li>
+            <Link to="/Hook">Hook</Link></li>
+            <li><Link to="/">Checkbox</Link></li>
+            <li><Link to="/Keyboard">Keyboard</Link></li>
+            <li> <Link to="/LoginPage">LoginPage</Link></li>
+            <li> <Link to="/Useeffect">Useeffect</Link></li>
+            <li> <Link to="/Welcome">Welcome</Link></li>
+        </ul>
            <h1> Welcome</h1>
         <h2>Hello Md</h2>
         
