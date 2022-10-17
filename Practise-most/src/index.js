54c1739d027192dba69b2d78aa9d24b758e628bb
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-17 20:54:06 +0530

    Update index.js

--- Practise-most/src/index.js
+++ Practise-most/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import  Headlines from './Components/Headlines'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
-  <App/>
-  </BrowserRouter>
-
+  // <BrowserRouter>
+  /* <App/> */
+  /* </BrowserRouter> */
+< Headlines/>
   /* </React.StrictMode> */
 );
 
