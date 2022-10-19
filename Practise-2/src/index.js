5f74620c5193660d1542a4e68e6bc248b61699f5
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-19 22:37:30 +0530

    Update index.js

--- Practise-2/src/index.js
+++ Practise-2/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Parent from './Components/Parent';
 import reportWebVitals from './reportWebVitals';
+import Counter from './Components/Counter.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    < Parent/>
-  </React.StrictMode>
+  // <React.StrictMode>
+  
+    <Counter/>
+  /* </React.StrictMode> */
 );
 
 
