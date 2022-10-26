8d848bbc29064f87dff51651612af5c4663b6c10
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-26 11:26:05 +0530

    Update Parent.jsx

--- Practise-2/src/Components/Parent.jsx
+++ Practise-2/src/Components/Parent.jsx
@@ -7,23 +7,22 @@ let data = createContext();
 function Parent() {
   let [name, setName] = useState("");
   return (
-    <>   <data.Provider value={name}>
-    <Child1 />
-  </data.Provider>
+    <>
   
-    <div>
-      <input
-        type="text"
-        onChange={(e) => {
-          setName(e.target.value);
-          localStorage.setItem("name",name)
-        }}
-      />
-      <h1>Parent</h1>
-      <li>
-        <Link to={"/child"}>Child</Link>
-      </li>
-    </div>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+            localStorage.setItem("name", name);
+          }}
+        />
+        <h1>Parent</h1>
+        <li>
+          <Link to={"/child"}>Child</Link>
+        </li>
+      </div>
     </>
   );
 }
