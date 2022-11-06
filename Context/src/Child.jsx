7f249fd3fffa64df55baaaa7e0e84b7fcba8d01b
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-11-06 21:41:33 +0530

    practice

--- Context/src/Child.jsx
+++ Context/src/Child.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useContext } from 'react'
 import {data,data1} from './App'
+import { RightOutlined  } from '@ant-design/icons'
+
+
 export default function Child() {
     let name = useContext(data)
     let gender = useContext(data1)
   return (
     <div>
+      <RightOutlined />
         <h1>My Name is {name} and my gender is  {gender} </h1>
     </div>
   )
--- Practise-2/src/Components/Child1.jsx
+++ Practise-2/src/Components/Child1.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useContext } from 'react'
 import{NameContext} from '../contexts/NameContext'
+import { FastForwardOutlined } from '@ant-design/icons'
 export default function Child1(n) {
   const {name} = useContext(NameContext)
   return (
     <div>
         <h1>Child1 name = {name}</h1>
+        <FastForwardOutlined  style={{color:"red"}}/>
         {console.log(NameContext)}
     </div>
   )
